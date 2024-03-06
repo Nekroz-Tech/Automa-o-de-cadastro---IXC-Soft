9fcda639800931053f38163eadb7138044c9c0eb
Vitor Pimentel <37230076+VitorPPimentel@users.noreply.github.com>
2024-03-06 12:00:08 -0300

    Update Automacao_Cad_IX-Soft.py

--- Automacao_Cad_IX-Soft.py
+++ Automacao_Cad_IX-Soft.py
@@ -1,34 +1,3 @@
-import importlib
-
-# Bibliotecas a serem verificadas
-#libs_to_check = ['time', 'pyautogui', 'tkinter', 'selenium', 'webdriver_manager', 'selenium.webdriver.chrome.service']
-
-# Verifique e instale as bibliotecas ausentes
-#for lib in libs_to_check:
-#    try:
-#        importlib.import_module(lib)
-#    except ImportError:
-#        print(f"{lib} não está instalado. Instalando...")
-#        try:
-#            if lib == 'selenium.webdriver.chrome.service':
-#                # Tratamento especial para 'selenium.webdriver.chrome.service'
-#                from selenium.webdriver.chrome.service import Service
-#            else:
-#                # Instalação padrão para outras bibliotecas
-#                import pip
-#                pip.main(['install', lib])
-#        except Exception as e:
-#            print(f"Falha ao instalar {lib}: {str(e)}")
-#        else:
-#            print(f"{lib} instalado com sucesso!")
-
-# Agora você pode usar as bibliotecas normalmente no restante do seu código
-
-import time
-import pyautogui
-import tkinter as tk
-
-#---------------------------------------------------------------
 from tkinter import *
 from tkinter import ttk
 from selenium import webdriver
@@ -43,21 +12,9 @@ servico = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=servico)
 
 
-#python -m PyInstaller --onefile Automacao_Cad_IX-Soft.py
+servico = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=servico)
 
-
-
-
-
-
-
-#imports
-#---------------------------------------------------------------
-
-#servico = Service(ChromeDriverManager().install())
-#driver = webdriver.Chrome(service=servico)
-#ptions = webdriver.ChromeOptions()
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.implicity_wait(10)
 
 
